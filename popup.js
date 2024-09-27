56811ba6cf66ddeaf0bdad706d8cda4baeee7bc3
JSCoder <97646262+JSCoder16@users.noreply.github.com>
2024-09-27 13:24:59 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,19 +1,23 @@
 // get are switch elements
 const zerk_back = document.getElementById("zerk-back");
 const auto_zerk = document.getElementById("auto-zerk");
-const zerk_five = document.getElementById("auto-500");
+const zerk_semi = document.getElementById("auto-500");
+const semi_rng = document.getElementById("semi-rng");
+
+// the element for display what the rating range is to berserk against in semi mode
+const ratingRageEl = document.getElementById("rng-display");
 
 // booleans to tell what is going on
 let isZerkingBack = false,
     isAutoZerking = false,
-    isSemiAuto = false;
+    isSemiAuto = false,
+    ratingRange = semi_rng.value;
 
 // set based off what is stored in chrome local storage
 chrome.storage.local.get("berserkBack").then((res) => {
     if(res.berserkBack !== undefined) {
         isZerkingBack = res.berserkBack;
         zerk_back.checked = isZerkingBack;
-        console.log("Berserking back")
     }
 });
 chrome.storage.local.get("autoBerserk").then((res) => {
@@ -25,7 +29,17 @@ chrome.storage.local.get("autoBerserk").then((res) => {
 chrome.storage.local.get("semiBerserk").then((res) => {
     if(res.semiBerserk !== undefined) {
         isSemiAuto = res.semiBerserk;
-        zerk_five.checked = isSemiAuto;
+        zerk_semi.checked = isSemiAuto;
+    }
+});
+chrome.storage.local.get("semiRange").then((res) => {
+    if(res.semiRange !== undefined) {
+        ratingRange = res.semiRange;
+        semi_rng.value = ratingRange;
+        console.log(ratingRange);
+        ratingRageEl.innerHTML = "-" + ratingRange;
+    }else {
+        chrome.storage.local.set({ "semiRange": ratingRange });
     }
 });
 
@@ -40,9 +54,15 @@ auto_zerk.addEventListener("click", (e) => {
     chrome.storage.local.set({ "autoBerserk": isAutoZerking });
 });
 
-zerk_five.addEventListener("click", (e) => {
+zerk_semi.addEventListener("click", (e) => {
     isSemiAuto = !isSemiAuto;
     chrome.storage.local.set({ "semiBerserk": isSemiAuto });
+});
+
+semi_rng.addEventListener("input", (e) => {
+    ratingRange = semi_rng.value;
+    ratingRageEl.innerHTML = "-" + ratingRange;
+    chrome.storage.local.set({ "semiRange": ratingRange });
 });
 
 
